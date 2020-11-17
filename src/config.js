d0109c9255b9c08158cb4b09a00395c8e76e18b9
akuehneBTG <53785083+akuehneBTG@users.noreply.github.com>
2020-11-16 22:39:10 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 const GITHUB_ISSUES_LINK =
   "https://api.github.com/repos/akuehneBTG/gh-pages-url-shortener/issues/";
 
-const PATH_SEGMENTS_TO_SKIP = 0;
+const PATH_SEGMENTS_TO_SKIP = 1;
 
 const HOST = "https://akuehneBTG.github.io/gh-pages-url-shortener/";
